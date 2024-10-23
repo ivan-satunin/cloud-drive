36b80f10c36bb1c4644542b3363c5f2c80e99326
Ivan Satunin <satuninivan@gmail.com>
2024-10-23 23:08:33 +0300

    feat: add User entity

---
+++ src/main/java/com/me/clouddrive/entity/User.java
@@ -0,0 +1,42 @@
+package com.me.clouddrive.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    private static final List<String> ROLES = List.of("USER");
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return ROLES.stream().map(SimpleGrantedAuthority::new).toList();
+    }
+}
